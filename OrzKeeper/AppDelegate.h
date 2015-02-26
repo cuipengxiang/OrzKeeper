11ee0978dd2c817c9d9d09fa625d0ffb104b4c21
Roc <cuipengxiang@gmail.com>
2015-02-27 00:04:11 +0800

    first commit

---
+++ OrzKeeper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrzKeeper
+//
+//  Created by Roc on 15/2/26.
+//  Copyright (c) 2015年 Roc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrzKeeper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OrzKeeper
+//
+//  Created by Roc on 15/2/26.
+//  Copyright (c) 2015年 Roc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
